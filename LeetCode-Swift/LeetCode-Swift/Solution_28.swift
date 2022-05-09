e47eab260a6c50768b2d0ac527a7de4815538d70
Wu Guoquan <wuguoquan@tp-link.com.cn>
2022-05-09 10:42:46 +0800

    新增 28.实现 strStr()

---
+++ LeetCode-Swift/LeetCode-Swift/Solution_28.swift
@@ -0,0 +1,48 @@
+//
+//  Solution_28.swift
+//  LeetCode-Swift
+//
+//  Created by 吴国权 on 2022/5/9.
+//
+
+import Foundation
+
+/*
+ 28.实现 strStr()
+ https://leetcode.cn/problems/implement-strstr/
+ */
+
+
+class Solution_28 {
+    func strStr(_ haystack: String, _ needle: String) -> Int {
+        guard !needle.isEmpty else {
+            return 0
+        }
+        
+        let haystackCount = haystack.count
+        let needleCount = needle.count
+        
+        guard haystackCount >= needleCount else {
+            return -1
+        }
+        
+        let iRange = 0...(haystackCount - needleCount)
+        
+        for i in iRange {
+            let startIndex = haystack.index(haystack.startIndex, offsetBy: i)
+            let endIndex = haystack.index(startIndex, offsetBy: needleCount)
+            let haystackRange = haystack[startIndex..<endIndex]
+            
+            if needle == haystackRange {
+                return i
+            } else {
+                continue
+            }
+        }
+        return -1
+    }
+    
+    func test() {
+        print(strStr("hello", "ll"))
+    }
+}
---
+++ LeetCode-Swift/LeetCode-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCode-Swift
+//
+//  Created by 吴国权 on 2022/5/9.
+//
+
+import Foundation
+
+let su = Solution_28()
+
+su.test()
