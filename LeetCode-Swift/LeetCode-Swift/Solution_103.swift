186d0d0d5e436a07c8cdf5e41e7167094f6fc0c4
Wu Guoquan <wuguoquan@tp-link.com.cn>
2022-05-10 11:18:04 +0800

    新增 103. 二叉树的锯齿形层序遍历（中等）

--- LeetCode-Swift/LeetCode-Swift/Solution_103.swift
+++ LeetCode-Swift/LeetCode-Swift/Solution_103.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+/*
+ 103. 二叉树的锯齿形层序遍历（中等）
+ https://leetcode.cn/problems/binary-tree-zigzag-level-order-traversal/
+ */
+
+class Solution_103 {
+    func zigzagLevelOrder(_ root: TreeNode?) -> [[Int]] {
+        guard root != nil else { return [] }
+        var queue = [root!]
+        var levelIndex = 0
+        var res = [[Int]]()
+            
+        while !queue.isEmpty {
+            let count = queue.count
+            var level = [Int]()
+            for _ in 0..<count {
+                let node = queue.removeFirst()
+                level.append(node.val)
+                if let left = node.left {
+                    queue.append(left)
+                }
+                if let right = node.right {
+                    queue.append(right)
+                }
+            }
+            res.append((levelIndex & 1 == 0) ? level : level.reversed())
+            levelIndex += 1
+        }
+        return res
+    }
+}
