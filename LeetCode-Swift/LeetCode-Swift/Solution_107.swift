2b4a3ab6b5592024158c7db50049e4e4add13527
Wu Guoquan <wuguoquan@tp-link.com.cn>
2022-05-10 10:42:55 +0800

    新增 107. 二叉树的层序遍历 II（中等）

--- LeetCode-Swift/LeetCode-Swift/Solution_107.swift
+++ LeetCode-Swift/LeetCode-Swift/Solution_107.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+/*
+ 107. 二叉树的层序遍历 II（中等）
+ https://leetcode.cn/problems/binary-tree-level-order-traversal-ii/
+ */
+
+class Solution_107 {
+    func levelOrderBottom(_ root: TreeNode?) -> [[Int]] {
+        func levelOrder(_ root: TreeNode?) -> [[Int]] {
+            guard root != nil else { return [] }
+            var queue = [root!]
+            var res = [[Int]]()
+            
+            while !queue.isEmpty {
+                let count = queue.count
+                var level = [Int]()
+                for _ in 0..<count {
+                    let node = queue.removeFirst()
+                    level.append(node.val)
+                    if let left = node.left {
+                        queue.append(left)
+                    }
+                    if let right = node.right {
+                        queue.append(right)
+                    }
+                }
+                res.append(level)
+            }
+            return res
+        }
+        var result = levelOrder(root)
+        result.reverse()
+        return result
+    }
+}
+
