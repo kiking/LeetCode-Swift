3db5d2ac3b68ae174e494ddc93af94e3560545d6
Wu Guoquan <wuguoquan@tp-link.com.cn>
2022-05-19 15:08:26 +0800

    新增 201. 数字范围按位与（中等）

--- LeetCode-Swift/LeetCode-Swift/Solution_201.swift
+++ LeetCode-Swift/LeetCode-Swift/Solution_201.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+/*
+ 201. 数字范围按位与（中等）
+ https://leetcode.cn/problems/bitwise-and-of-numbers-range/
+ */
+
+    // m 5 1 0 1
+    //   6 1 1 0
+    // n 7 1 1 1
+    // 把n右边依次去掉1，直到<=m
+    // m 5 1 0 1
+    //   6 1 1 0
+    // n 7 1 0 0
+// n&m
+
+class Solution_201 {
+    func rangeBitwiseAnd(_ left: Int, _ right: Int) -> Int {
+        var num = right
+        while left < num {
+            num = num&(num - 1)
+        }
+        return left&num
+    }
+}
