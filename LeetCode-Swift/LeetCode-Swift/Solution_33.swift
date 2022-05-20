5cdf5e5424a12d0d5ff4f283ef281c9854bada75
Wu Guoquan <wuguoquan@tp-link.com.cn>
2022-05-20 16:17:41 +0800

    新增 33. 搜索旋转排序数组（中等）

--- LeetCode-Swift/LeetCode-Swift/Solution_33.swift
+++ LeetCode-Swift/LeetCode-Swift/Solution_33.swift
@@ -6,3 +6,46 @@
 //
 
 import Foundation
+
+/*
+ 33. 搜索旋转排序数组（中等）
+ https://leetcode.cn/problems/search-in-rotated-sorted-array/
+ */
+
+class Solution_33 {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        if nums.count == 0 {
+            return -1
+        } else if nums.count == 1 {
+            return nums[0] == target ? 0 : -1
+        }
+        
+        var start = 0
+        var end = nums.count - 1
+        
+        while start + 1 < end {
+            let mid = start + (end - start) / 2
+            if nums[start] <= nums[mid] {
+                if target <= nums[mid] && target >= nums[start] {
+                    end = mid
+                } else {
+                    start = mid
+                }
+            } else {
+                if target >= nums[mid] && target <= nums[end] {
+                    start = mid
+                } else {
+                    end = mid
+                }
+            }
+        }
+
+        if nums[start] == target {
+            return start
+        } else if nums[end] == target {
+            return end
+        }
+
+        return -1
+    }
+}
